aea65f9fc80c492ff58828a991aa3b258abc25f5
Fatih Demir <101219059+fatih260@users.noreply.github.com>
2023-12-31 13:10:53 +0300

    part2

--- main.py
+++ main.py
@@ -85,8 +85,96 @@ def run_knight_tour_simulation(p, trials, board_size=8):
     # Call the function to write the results to a file
     write_results_to_file(results, p, board_size)
 
+
+def knight_tour_deterministic(p, k, trials, board_size=8):
+    """Run the knight's tour with a random approach for k steps, then use backtracking."""
+    results = []
+    threshold_squares = int(p * board_size ** 2)  # Calculate the threshold in terms of squares
+    
+    for trial in range(trials):
+        board = [[-1 for _ in range(board_size)] for _ in range(board_size)]
+        move_sequence = []
+        current_pos = (random.randrange(board_size), random.randrange(board_size))
+        board[current_pos[0]][current_pos[1]] = 0  # mark the start position as visited
+        move_sequence.append(current_pos)  # record the start position
+        
+        # Randomly move k steps
+        for _ in range(k):
+            legal_moves = generate_legal_moves(current_pos[0], current_pos[1], board)
+            if not legal_moves:
+                break  # No more legal moves
+            current_pos = random.choice(legal_moves)
+            board[current_pos[0]][current_pos[1]] = len(move_sequence)
+            move_sequence.append(current_pos)
+
+        # Use backtracking to complete the tour
+        success = backtracking_tour(board, move_sequence, threshold_squares)
+        
+        results.append({
+            'start': move_sequence[0],
+            'move_sequence': move_sequence,
+            'success': success,
+            'tour_length': len(move_sequence),
+            'board': board
+        })
+    
+    return results
+
+def backtracking_tour(board, move_sequence, threshold_squares):
+    """Use backtracking to complete the knight's tour."""
+    if len(move_sequence) - 1 >= threshold_squares:
+        return True  # The tour is already successful
+
+    current_pos = move_sequence[-1]
+    legal_moves = generate_legal_moves(current_pos[0], current_pos[1], board)
+
+    for move in legal_moves:
+        move_sequence.append(move)
+        board[move[0]][move[1]] = len(move_sequence) - 1  # mark the square as visited
+        if backtracking_tour(board, move_sequence, threshold_squares):
+            return True  # Continue the tour
+        # If the tour is unsuccessful, backtrack
+        move_sequence.pop()
+        board[move[0]][move[1]] = -1
+    
+    return False  # No successful tour found
+
+def run_knight_tour_simulation_det(p, k_values, trials, board_size=8):
+    """Run the knight's tour simulation for different values of k and print the summary of results."""
+    for k in k_values:
+        successful_tours = 0
+        results = knight_tour_deterministic(p, k, trials, board_size)
+        
+        # Count the number of successful tours
+        for result in results:
+            if result['success']:
+                successful_tours += 1
+        
+        # Calculate the probability of a successful tour
+        probability_of_success = successful_tours / trials
+        
+        # Print the summary in the required format
+        print(f"LasVegas Algorithm With p = {p}, k = {k}")
+        print(f"Number of successful tours: {successful_tours}")
+        print(f"Number of trials: {trials}")
+        print(f"Probability of a successful tour: {probability_of_success:.5f}\n")
+
+    write_results_to_file(results, p, board_size)
+
 # Run the simulation for different values of p and print the results
-for p_value in [0.7, 0.8, 0.85]:
-    trial_count = 10  # Use 100000 for the actual project run
-    run_knight_tour_simulation(p_value, trial_count)
-    break
+input_type = input("Enter part1 for Las Vegas Algorithm or part2 for Las Vegas Algorithm with Deterministic Algorithm: ")
+
+if input_type == "part1":
+    for p_value in [0.7, 0.8, 0.85]:
+        trial_count = 10  # Use 100000 for the actual project run
+        run_knight_tour_simulation(p_value, trial_count)
+        break
+
+elif input_type == "part2":
+        # Run the simulation for different values of p, k, and print the results
+    p_values = [0.7, 0.8, 0.85]
+    k_values = [0, 1, 2, 3]
+    trial_count = 10000
+
+    #for p_value in p_values:
+    #run_knight_tour_simulation_det(0.85, k_values, trial_count)
